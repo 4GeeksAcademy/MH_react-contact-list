acf79f7a0f1ec6f8a0f43e06927fb065991ccf0f
mhavliczek <mhavliczek@gmail.com>
2024-10-13 23:05:10 -0300

    añadir contactos

--- src/views/AddContact.jsx
+++ src/views/AddContact.jsx
@@ -3,39 +3,114 @@ import { Link } from 'react-router-dom';
 import { Context } from '../store/appContext';
 import Swal from 'sweetalert2';
 
+
+
 export const AddContact = () => {
   const { store, actions } = useContext(Context);
+  
+ 
   const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
 
-  const handleSubmit = async () => {
-    let contacto = {
-      name: 'Pancho',
-      phone: '912345678',
-      email: 'pancho@capone.cl',
-      address: 'Elm Street 13'
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const data = {
+      name,
+      phone,
+      email,
+      address,
     };
 
-    let response = await actions.createContact(contacto);
+    const success = await actions.createContact(data);
 
-    if (response) {
-      Swal.fire('Contacto agregado con éxito!');
-      //Limpiar los estados
+    if (success) {
+      Swal.fire('Exito!', 'Contact agregado correctamente!', 'exito');
+      setName('');
+      setPhone('');
+      setEmail('');
+      setAddress('');
     } else {
-      Swal.fire('Error al agregar contacto');
-      //Mantener valores de los estados
+      Swal.fire('Error!', 'Error .', 'error');
     }
-  }
+  };
 
   return (
-    <>
-      <div>Formulario con campos para agregar un nuevo contacto</div>
-      <button className='btn btn-success' onClick={ () => { handleSubmit() }}>Agregar contacto</button>
-      <Link to="/">
-        <button className='btn btn-warning'>Volver</button>
-      </Link>
-    </>
+        <div className="container">
+      <div className="row p-0">
+        <div
+          className="col h4"
+          style={{
+           
+            borderRadius: "3px",
+            padding: "40px",
+            background: "white",
+            color: "black"
+          }}
+        >
+          <form onSubmit={handleSubmit}>
+            {/* Form fields */}
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Nombre
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phone" className="form-label">
+                Teléfono
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="address" className="form-label">
+                Dirección
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+            <div className="d-flex justify-content-end">
+              <button type="submit" className="btn btn-primary m-1">
+                Submit
+              </button>
+              <Link to="/" className="btn btn-success" style={{ marginLeft: '10px' }}>
+                Ir a Lista Conctactos
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
